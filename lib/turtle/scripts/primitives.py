f34aea5ca6a2406058551d26f1ebc76409a15464
richeyrose <41282797+richeyrose@users.noreply.github.com>
2019-12-22 09:36:39 +0000

    openlock triangular base script written

--- lib/turtle/scripts/primitives.py
+++ lib/turtle/scripts/primitives.py
@@ -1,6 +1,8 @@
 import bpy
+from math import sqrt, cos, radians, acos, degrees
 
 
+# TODO: make it consistent whether we add turtle in script or prior
 def draw_cuboid(size):
     """Returns a cuboid. size = (x, y, z)"""
     turtle = bpy.context.scene.cursor
@@ -19,3 +21,21 @@ def draw_cuboid(size):
     t.select_all()
     bpy.ops.mesh.normals_make_consistent()
     return bpy.context.object
+
+
+def draw_triangle(b, c, A):
+    '''draws a triangle given the length of two sides (a, b) and the angle between them (A)'''
+    a = sqrt((b**2 + c**2) - ((2 * b * c) * cos(radians(A))))
+    B = degrees(acos((c**2 + a**2 - (b**2)) / (2 * c * a)))
+    C = 180 - A - B
+
+    t = bpy.ops.turtle
+    t.begin_path()
+    t.fd(d=b)
+    t.rt(d=180 - C)
+    t.fd(d=a)
+    t.rt(d=180 - B)
+    t.fd(d=c)
+    t.select_path()
+    t.merge()
+    t.pu()

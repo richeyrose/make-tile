6f738dedb23c15cb6da5a1c222479e9b97a3a101
richeyrose <41282797+richeyrose@users.noreply.github.com>
2020-09-26 08:56:51 +0100

    Rewrote exporter for mono mesh

--- operators/exporter.py
+++ operators/exporter.py
@@ -3,7 +3,6 @@ from random import random
 import bpy
 from bpy.types import Panel
 from .. utils.registration import get_prefs
-from .. enums.enums import units
 from .voxeliser import voxelise
 from .. lib.utils.collections import get_objects_owning_collections
 from . bakedisplacement import (
@@ -49,203 +48,172 @@ class MT_OT_Export_Tile_Variants(bpy.types.Operator):
     bl_options = {'REGISTER'}
 
     def execute(self, context):
-        objects = bpy.data.objects
-        scene_props = context.scene.mt_scene_props
-        selected_objects = context.selected_objects.copy()
-        visible_objects = []
-
-        for obj in bpy.data.objects:
-            obj.select_set(False)
-
-        tile_collections = set()
-
         # set up exporter options
-        blend_units = scene_props.export_units
-        export_path = scene_props.export_path
+        prefs = get_prefs()
+        scene_props = context.scene.mt_scene_props
+
+        #number of variants we will generate
         num_variants = scene_props.num_variants
-        randomise = scene_props.randomise_on_export
-        voxelise = scene_props.voxelise_on_export
 
-        if blend_units == 'CM':
-            unit_multiplier = 10
-        if blend_units == 'INCHES':
-            unit_multiplier = 25.4
+        # set cycles to bake mode and store original settings
+        orig_settings = set_cycles_to_bake_mode()
 
+        # voxelise options
+        voxelise_on_export = scene_props.voxelise_on_export
+
+        # ensure export path exists
+        export_path = prefs.default_export_path
         if not os.path.exists(export_path):
             os.mkdir(export_path)
 
-        for obj in selected_objects:
+        # Controls if we rescale on export
+        blend_units = scene_props.export_units
+        if blend_units == 'CM':
+            unit_multiplier = 10
+        elif blend_units == 'INCHES':
+            unit_multiplier = 25.4
+        else:
+            unit_multiplier = 1
+
+        objects = bpy.data.objects
+        visible_objects = []
+
+        # get list of tile collections our selected objects are in. We export
+        # all visible objects in the collections
+        tile_collections = set()
+        for obj in context.selected_objects:
             obj_collections = get_objects_owning_collections(obj.name)
 
             for collection in obj_collections:
-                tile_collections.add(collection)
-
-        orig_settings = set_cycles_to_bake_mode()
+                if collection.mt_tile_props.is_mt_collection is True:
+                    tile_collections.add(collection)
 
         for collection in tile_collections:
-            preview_obs = set()
+            visible_objects = []
 
             for obj in collection.objects:
-                if obj.mt_object_props.geometry_type == 'PREVIEW':
-                    preview_obs.add(obj)
-
-                if obj.visible_get() is True:
+                if obj.type == 'MESH' and obj.visible_get() is True:
                     visible_objects.append(obj)
+
+            # we will generate variants of preview obs equal to num_variants
+            displacement_obs = []
+            for obj in visible_objects:
+                if obj.mt_object_props.geometry_type == 'PREVIEW':
+                    displacement_obs.append(obj)
+                if obj.mt_object_props.geometry_type == 'DISPLACEMENT':
+                    set_to_preview(obj)
+                    displacement_obs.append(obj)
 
             i = 0
             while i < num_variants:
-                disp_obs = []
+                # construct a random name for our variant
+                file_path = os.path.join(
+                    export_path,
+                    collection.name + '.' + str(random()) + '.stl')
 
-                for obj in preview_obs:
-                    obj.hide_viewport = False
-                    disp_obj = obj.mt_object_props.linked_object
+                # generate a random variant for each displacement object
+                for obj in displacement_obs:
+                    obj_props = obj.mt_object_props
+                    ctx = {
+                        'selected_objects': [obj],
+                        'selected_editable_objects': [obj],
+                        'active_object': obj,
+                        'object': obj}
 
                     for item in obj.material_slots.items():
                         if item[0]:
                             material = bpy.data.materials[item[0]]
                             tree = material.node_tree
-                        if 'Seed' in tree.nodes:
-                            seed_node = tree.nodes['Seed']
-                            rand_seed = random()
-                            seed_node.outputs[0].default_value = rand_seed * 1000
-                    if disp_obj not in visible_objects:
-                        disp_obj_is_visible = False
-                        disp_obj.hide_viewport = False
-                        disp_obj.hide_set(False)
-                    else:
-                        disp_obj_is_visible = True
+                            if 'Seed' in tree.nodes:
+                                rand = random()
+                                seed_node = tree.nodes['Seed']
+                                seed_node.outputs[0].default_value = rand * 1000
 
-
-                    # Bake displacement map and apply to displacement obj
-                    disp_image, disp_obj = bake_displacement_map(obj, disp_obj)
-                    disp_texture = disp_obj['disp_texture']
+                    disp_image, obj = bake_displacement_map(obj)
+                    tile_props = collection.mt_tile_props
+                    disp_strength = tile_props.displacement_strength
+                    disp_texture = obj['disp_texture']
                     disp_texture.image = disp_image
-                    disp_mod = disp_obj.modifiers[disp_obj['disp_mod_name']]
+                    disp_mod = obj.modifiers[obj['disp_mod_name']]
                     disp_mod.texture = disp_texture
                     disp_mod.mid_level = 0
-                    disp_mod.strength = collection.mt_tile_props.displacement_strength
-                    subsurf_mod = disp_obj.modifiers[disp_obj['subsurf_mod_name']]
-                    subsurf_mod.levels = bpy.context.scene.mt_scene_props.subdivisions
-                    disp_obs.append(disp_obj)
+                    disp_mod.strength = disp_strength
 
-                obj_copies = []
+                    subsurf_mod = obj.modifiers[obj['subsurf_mod_name']]
+                    subsurf_mod.levels = scene_props.subdivisions
+                    bpy.ops.object.modifier_move_to_index(ctx, modifier=subsurf_mod.name, index=0)
+                    obj_props.geometry_type = 'DISPLACEMENT'
 
-                depsgraph = context.evaluated_depsgraph_get()
-                # create copy of each displacement object and apply modifiers
-                for obj in disp_obs:
-                    object_eval = obj.evaluated_get(depsgraph)
-                    mesh_from_eval = bpy.data.meshes.new_from_object(object_eval)
-                    dup_obj = bpy.data.objects.new("dupe", mesh_from_eval)
-                    dup_obj.mt_object_props.geometry_type = 'DISPLACEMENT'
+                if voxelise_on_export:
+                    depsgraph = context.evaluated_depsgraph_get()
+                    dupes = []
 
-                    # set dupe objects locrotscale and parent to original object
-                    dup_obj.location = obj.location
-                    dup_obj.rotation_euler = obj.rotation_euler
-                    dup_obj.scale = obj.scale
-                    dup_obj.parent = obj.parent
-                    # collection.objects.link(dup_obj)
-                    obj_copies.append(dup_obj)
+                    for obj in visible_objects:
+                        object_eval = obj.evaluated_get(depsgraph)
+                        mesh_from_eval = bpy.data.meshes.new_from_object(object_eval)
+                        dup_obj = bpy.data.objects.new('dupe', mesh_from_eval)
+                        dup_obj.location = obj.location
+                        dup_obj.rotation_euler = obj.rotation_euler
+                        dup_obj.scale = obj.scale
+                        dup_obj.parent = obj.parent
+                        collection.objects.link(dup_obj)
+                        dupes.append(dup_obj)
 
-                    # hide original, resetting disp modifiers if disp_object was not originally visible
-                    if disp_obj_is_visible is False:
-                        set_to_preview(obj)
-                    obj.hide_viewport = True
-
-
-                # create copy of visible objects that are not displacement or preview objects and apply modifiers
-                depsgraph = context.evaluated_depsgraph_get()
-                for obj in collection.all_objects:
-                    if obj.type == 'MESH':
-                        if obj.mt_object_props.geometry_type not in ('PREVIEW', 'DISPLACEMENT') and obj.visible_get() is True:
-                            object_eval = obj.evaluated_get(depsgraph)
-                            mesh_from_eval = bpy.data.meshes.new_from_object(object_eval)
-                            dup_obj = bpy.data.objects.new("dupe", mesh_from_eval)
-                            dup_obj.location = obj.location
-                            dup_obj.rotation_euler = obj.rotation_euler
-                            dup_obj.scale = obj.scale
-                            dup_obj.parent = obj.parent
-                            # collection.objects.link(dup_obj)
-                            obj_copies.append(dup_obj)
-
-                for obj in obj_copies:
-                    collection.objects.link(obj)
-
-                # Voxelise
-                if context.scene.mt_voxelise_on_export is True:
-                    # Merge
-                    if context.scene.mt_merge:
+                    # join dupes together
+                    if len(dupes) > 0:
                         ctx = {
-                            'object': obj_copies[0],
-                            'active_object': obj_copies[0],
-                            'selected_objects': obj_copies,
-                            'selected_editable_objects': obj_copies
-                        }
-
-                        #TODO: Optimise join https://blender.stackexchange.com/questions/50160/scripting-low-level-join-meshes-elements-hopefully-with-bmesh
+                            'object': dupes[0],
+                            'active_object': dupes[0],
+                            'selected_objects': dupes,
+                            'selected_editable_objects': dupes}
                         bpy.ops.object.join(ctx)
-                        del obj_copies[1:]
 
-                    for obj in obj_copies:
-                        obj.data.remesh_voxel_size = bpy.context.scene.mt_voxel_size
-                        obj.data.remesh_voxel_adaptivity = bpy.context.scene.mt_voxel_adaptivity
-                        ctx = {
-                            'object': obj,
-                            'active_object': obj,
-                            'selected_objects': [obj],
-                            'selected_editable_objects': [obj]
-                        }
+                    voxelise(dupes[0])
 
-                        bpy.ops.object.voxel_remesh(ctx)
-                        obj.mt_object_props.geometry_type = 'VOXELISED'
+                    ctx = {
+                        'object': dupes[0],
+                        'active_object': dupes[0],
+                        'selected_objects': [dupes[0]],
+                        'selected_editable_objects': [dupes[0]]}
 
-                # construct filepath
-                file_path = os.path.join(
-                    context.scene.mt_export_path,
-             objn.name + '.' + str(random()) + '.stl')
+                    # export our object
+                    bpy.ops.export_mesh.stl(
+                        ctx,
+                        filepath=file_path,
+                        check_existing=True,
+                        filter_glob="*.stl",
+                        use_selection=True,
+                        global_scale=unit_multiplier,
+                        use_mesh_modifiers=True)
 
-                ctx = {
-                    'selected_objects': obj_copies,
-                    'selected_editable_objects': obj_copies,
-                    'object': obj_copies[0],
-                    'active_object': obj_copies[0]
-                }
+                    objects.remove(dupes[0], do_unlink=True)
 
-                # export our tile
-                bpy.ops.export_mesh.stl(
-                    ctx,
-                    filepath=file_path,
-                    check_existing=True,
-                    filter_glob="*.stl",
-                    use_selection=True,
-                    global_scale=unit_multiplier,
-                    use_mesh_modifiers=True)
+                    # clean up orphaned meshes
+                    for mesh in bpy.data.meshes:
+                        if mesh.users == 0:
+                            bpy.data.meshes.remove(mesh)
+                else:
+                    ctx = {
+                        'object': visible_objects[0],
+                        'active_object': visible_objects[0],
+                        'selected_objects': visible_objects,
+                        'selected_editable_objects': visible_objects}
 
-                # Delete copies
-                for obj in obj_copies:
-                    # obj.select_set(False)
-                    objects.remove(obj, do_unlink=True)
+                    # export our object
+                    bpy.ops.export_mesh.stl(
+                        ctx,
+                        filepath=file_path,
+                        check_existing=True,
+                        filter_glob="*.stl",
+                        use_selection=True,
+                        global_scale=unit_multiplier,
+                        use_mesh_modifiers=True)
 
-                # unhide preview objects
-                for obj in preview_obs:
-                    obj.hide_viewport = False
-                    obj.hide_set(False)
-
+                for obj in displacement_obs:
+                    set_to_preview(obj)
                 i += 1
 
-            for obj in preview_obs:
-                obj.select_set(False)
-                obj.hide_viewport = True
-
         reset_renderer_from_bake(orig_settings)
-
-        # select original objects
-        for obj in selected_objects:
-            obj.select_set(True)
-
-        # unhide original visible objects
-        for obj in visible_objects:
-            obj.hide_viewport = False
-            obj.hide_set(False)
 
         return {'FINISHED'}
 

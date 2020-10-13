ce80d3de569e7432b24afc32375e0d902cd5b366
Richard Rose <richard.rose@modmodterrain.com>
2020-10-13 09:11:42 +0100

    object_rescaler.py rewritten to scale based on cursor location

--- operators/object_rescaler.py
+++ operators/object_rescaler.py
@@ -5,6 +5,7 @@ class MT_OT_Rescale_Object(bpy.types.Operator):
     '''Scales an object down to blender units'''
     bl_idname = "object.mt_rescale_object"
     bl_label = "Rescale Object"
+    bl_description = "Scale object down for use with MakeTile's material system"
     bl_options = {'REGISTER', 'UNDO'}
 
     @classmethod
@@ -15,13 +16,13 @@ class MT_OT_Rescale_Object(bpy.types.Operator):
     def execute(self, context):
         scene_props = context.scene.mt_scene_props
         base_unit = scene_props.base_unit
+        cursor_loc = context.scene.cursor.location
 
-        for obj in context.selected_editable_objects:
-            if base_unit == 'INCHES':
-                obj.scale = obj.scale * 0.039701
-            if base_unit == 'CM':
-                obj.scale = obj.scale * 0.01
+        if base_unit == 'INCHES':
+            bpy.ops.transform.resize(value=(0.039701, 0.039701, 0.039701), orient_type='GLOBAL', center_override=cursor_loc)
+        if base_unit == 'CM':
+            bpy.ops.transform.resize(value=(0.01, 0.01, 0.01), orient_type='GLOBAL', center_override=cursor_loc)
 
-        bpy.ops.object.transform_apply()
+        bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
 
         return {'FINISHED'}

6daff87b08d3a1917f09499548b00ad873bd053b
Richard Rose <richard.rose@modmodterrain.com>
2020-10-16 15:20:24 +0100

    save and restore current lock_scale state when rescaling objects

--- operators/object_rescaler.py
+++ operators/object_rescaler.py
@@ -18,11 +18,26 @@ class MT_OT_Rescale_Object(bpy.types.Operator):
         base_unit = scene_props.base_unit
         cursor_loc = context.scene.cursor.location
 
+        # save current scale lock state
+        locked = []
+        for obj in context.selected_objects:
+            locked.append((obj, obj.lock_scale[0], obj.lock_scale[1], obj.lock_scale[2]))
+            obj.lock_scale[0] = False
+            obj.lock_scale[1] = False
+            obj.lock_scale[2] = False
+
+        # scale objects
         if base_unit == 'INCHES':
             bpy.ops.transform.resize(value=(0.039701, 0.039701, 0.039701), orient_type='GLOBAL', center_override=cursor_loc)
         if base_unit == 'CM':
             bpy.ops.transform.resize(value=(0.01, 0.01, 0.01), orient_type='GLOBAL', center_override=cursor_loc)
 
         bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
+
+        # restore original scale lock state
+        for ob in locked:
+            ob[0].lock_scale[0] = ob[1]
+            ob[0].lock_scale[1] = ob[2]
+            ob[0].lock_scale[2] = ob[3]
 
         return {'FINISHED'}

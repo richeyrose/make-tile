db2b00c9ac0013acdddd23aa1df2b6aacecfceb2
Richard Rose <richard.rose@modmodterrain.com>
2021-05-31 07:04:41 +0100

    Semi_circ tiles redone

--- tile_creation/Semi_Circ_Tiles.py
+++ tile_creation/Semi_Circ_Tiles.py
@@ -7,10 +7,17 @@ from mathutils.geometry import intersect_line_line
 import bpy
 import bmesh
 from bpy.types import Operator, Panel
+from bpy.props import (
+    EnumProperty,
+    FloatProperty,
+    StringProperty)
+
+from ..properties.properties import (
+    create_base_blueprint_enums,
+    create_main_part_blueprint_enums)
 
 from . create_tile import (
     convert_to_displacement_core,
-    finalise_tile,
     spawn_empty_base,
     spawn_prefab,
     set_bool_obj_props,
@@ -53,7 +60,6 @@ from ..lib.bmturtle.commands import (
     dn,
     arc)
 
-
 class MT_PT_Semi_Circ_Floor_Panel(Panel):
     """Draw a tile options panel in UI."""
 
@@ -80,7 +86,7 @@ class MT_PT_Semi_Circ_Floor_Panel(Panel):
 
         layout.label(text="Blueprints")
         layout.prop(scene_props, 'base_blueprint')
-        layout.prop(scene_props, 'main_part_blueprint')
+        layout.prop(scene_props, 'main_part_blueprint', text="Main")
 
         layout.label(text="Tile Properties")
         layout.prop(scene_props, 'tile_z', text="Height")
@@ -100,39 +106,111 @@ class MT_PT_Semi_Circ_Floor_Panel(Panel):
         layout.operator('scene.reset_tile_defaults')
 
 
-class MT_OT_Make_Semi_Circ_Floor_Tile(MT_Tile_Generator, Operator):
+class MT_OT_Make_Semi_Circ_Floor_Tile(Operator, MT_Tile_Generator):
     """Create a Semi Circular Floor Tile."""
 
     bl_idname = "object.make_semi_circ_floor"
     bl_label = "Semi Circular Floor"
-    bl_options = {'UNDO'}
+    bl_options = {'UNDO', 'REGISTER'}
     mt_blueprint = "CUSTOM"
     mt_type = "SEMI_CIRC_FLOOR"
 
+    def update_base_blueprint_enums(self, context):
+        if not self.invoked:
+            if self.base_blueprint in ("OPENLOCK", "PLAIN"):
+                self.base_y = 0.5
+                self.base_z = 0.2755
+            else:
+                self.base_y = self.tile_y
+                self.base_z = 0.0
+
+    main_part_blueprint: EnumProperty(
+        items=create_main_part_blueprint_enums,
+        name="Wall")
+
+    base_blueprint: EnumProperty(
+        items=create_base_blueprint_enums,
+        update=update_base_blueprint_enums,
+        name="Base"
+    )
+
+    # used for curved floors
+    curve_type: EnumProperty(
+        items=[
+            ("POS", "Positive", ""),
+            ("NEG", "Negative", "")],
+        name="Curve type",
+        default="POS",
+        description="Whether the tile has a positive or negative curvature"
+    )
+
+    base_radius: FloatProperty(
+        name="Base inner radius",
+        default=2.0,
+        step=50,
+        precision=1,
+        min=0,
+    )
+
+    angle: FloatProperty(
+        name="Base Angle",
+        default=90,
+        step=5,
+        precision=1
+    )
+
+
     def execute(self, context):
         """Execute the operator."""
-        scene = context.scene
-        scene_props = scene.mt_scene_props
-        base_blueprint = scene_props.base_blueprint
-        core_blueprint = scene_props.main_part_blueprint
+        super().execute(context)
+        if not self.refresh:
+            return {'PASS_THROUGH'}
+
+        base_blueprint = self.base_blueprint
+        core_blueprint = self.main_part_blueprint
         base_type = 'SEMI_CIRC_BASE'
         core_type = 'SEMI_CIRC_FLOOR_CORE'
-
-        cursor_orig_loc, cursor_orig_rot = initialise_floor_creator(
-            context, scene_props)
         subclasses = get_all_subclasses(MT_Tile_Generator)
-        base = spawn_prefab(context, subclasses, base_blueprint, base_type)
+
+        kwargs = {"tile_name": self.tile_name}
+        base = spawn_prefab(context, subclasses, base_blueprint, base_type, **kwargs)
+        kwargs["base_name"] = base.name
 
         if core_blueprint == 'NONE':
             preview_core = None
         else:
-            preview_core = spawn_prefab(context, subclasses, core_blueprint, core_type)
+            preview_core = spawn_prefab(context, subclasses, core_blueprint, core_type, **kwargs)
 
-        finalise_tile(base, preview_core, cursor_orig_loc, cursor_orig_rot)
+        self.finalise_tile(context, base, preview_core)
 
-        # scene.render.engine = original_renderer
         return {'FINISHED'}
 
+    def init(self, context):
+        super().init(context)
+        tile_collection = bpy.data.collections[self.tile_name]
+        tile_props = tile_collection.mt_tile_props
+        tile_props.collection_type = "TILE"
+        tile_props.tile_size = (self.tile_x, self.tile_y, self.tile_z)
+        tile_props.base_size = (self.base_x, self.base_y, self.base_z)
+
+    def draw(self, context):
+        super().draw(context)
+        layout = self.layout
+        layout.prop(self, 'tile_material_1')
+        layout.prop(self, 'base_blueprint')
+        layout.prop(self, 'main_part_blueprint', text="Main")
+
+        layout.label(text="Tile Properties")
+        layout.prop(self, 'tile_z', text="Height")
+        layout.prop(self, 'base_radius', text="Radius")
+        layout.prop(self, 'angle', text="Angle")
+        layout.prop(self, 'curve_type', text="Curve Type")
+
+        layout.label(text="Sync Proportions")
+        layout.prop(self, 'z_proportionate_scale', text="Height")
+
+        layout.label(text="Base Properties")
+        layout.prop(self, 'base_z', text="Height")
 
 class MT_OT_Make_Openlock_Semi_Circ_Base(MT_Tile_Generator, Operator):
     """Internal Operator. Generate an OpenLOCK semi circular base."""
@@ -145,8 +223,7 @@ class MT_OT_Make_Openlock_Semi_Circ_Base(MT_Tile_Generator, Operator):
 
     def execute(self, context):
         """Execute the operator."""
-        tile = context.collection
-        tile_props = tile.mt_tile_props
+        tile_props = bpy.data.collections[self.tile_name].mt_tile_props
         spawn_openlock_base(self, tile_props)
         return{'FINISHED'}
 
@@ -162,7 +239,7 @@ class MT_OT_Make_Plain_Semi_Circ_Base(MT_Tile_Generator, Operator):
 
     def execute(self, context):
         """Execute the operator."""
-        tile_props = context.collection.mt_tile_props
+        tile_props = bpy.data.collections[self.tile_name].mt_tile_props
         spawn_plain_base(self, tile_props)
         return{'FINISHED'}
 
@@ -178,8 +255,7 @@ class MT_OT_Make_Empty_Semi_Circular_Base(MT_Tile_Generator, Operator):
 
     def execute(self, context):
         """Execute the operator."""
-        tile = context.collection
-        tile_props = tile.mt_tile_props
+        tile_props = bpy.data.collections[self.tile_name].mt_tile_props
         spawn_empty_base(tile_props)
         return{'FINISHED'}
 
@@ -192,11 +268,11 @@ class MT_OT_Make_Plain_Semi_Circ_Floor_Core(MT_Tile_Generator, Operator):
     bl_options = {'INTERNAL'}
     mt_blueprint = "PLAIN"
     mt_type = "SEMI_CIRC_FLOOR_CORE"
+    base_name: StringProperty()
 
     def execute(self, context):
         """Execute the operator."""
-        tile = context.collection
-        tile_props = tile.mt_tile_props
+        tile_props = bpy.data.collections[self.tile_name].mt_tile_props
         spawn_plain_floor_cores(self, tile_props)
         return{'FINISHED'}
 
@@ -209,11 +285,11 @@ class MT_OT_Make_Openlock_Semi_Circ_Floor_Core(MT_Tile_Generator, Operator):
     bl_options = {'INTERNAL'}
     mt_blueprint = "OPENLOCK"
     mt_type = "SEMI_CIRC_FLOOR_CORE"
+    base_name: StringProperty()
 
     def execute(self, context):
         """Execute the operator."""
-        tile = context.collection
-        tile_props = tile.mt_tile_props
+        tile_props = bpy.data.collections[self.tile_name].mt_tile_props
         spawn_plain_floor_cores(self, tile_props)
         return{'FINISHED'}
 
@@ -226,6 +302,7 @@ class MT_OT_Make_Empty_Semi_Circ_Floor_Core(MT_Tile_Generator, Operator):
     bl_options = {'INTERNAL'}
     mt_blueprint = "NONE"
     mt_type = "SEMI_CIRC_FLOOR_CORE"
+    base_name: StringProperty()
 
     def execute(self, context):
         """Execute the operator."""
@@ -395,6 +472,7 @@ def spawn_plain_floor_cores(self, tile_props):
 
     convert_to_displacement_core(
         core,
+        tile_props,
         textured_vertex_groups)
     return core
 

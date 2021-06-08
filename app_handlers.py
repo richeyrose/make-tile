ad8f8d73148bab1039fb587be927118f56deba30
Richard Rose <richard.rose@modmodterrain.com>
2021-06-08 16:50:53 +0100

    scene props now initialised properly on addon installation and new file load

--- app_handlers.py
+++ app_handlers.py
@@ -10,15 +10,16 @@ from .materials.materials import (
 
 def create_properties_on_activation(dummy):
     bpy.app.handlers.depsgraph_update_pre.remove(create_properties_on_activation)
-    #create_properties()
     load_material_libraries()
+    context = bpy.context
+    initialise_scene_props(context)
 
 
 @persistent
 def create_properties_on_load(dummy):
-    create_properties()
     load_material_libraries()
-
+    context = bpy.context
+    initialise_scene_props(context)
 
 def load_material_libraries():
     prefs = get_prefs()
@@ -50,6 +51,7 @@ def get_tile_type(tile_type):
 # when the appropriate undo step is reached.
 # Therefore here we check to see if our tile_defaults custom scene property is being
 # unloaded and if it is we rerun create_properties_on_load
+# I don;t think this is needed any more.
 '''
 @persistent
 def recreate_properties_on_undo(dummy):
@@ -97,18 +99,8 @@ def update_mt_scene_props_handler(dummy):
         pass
 
 
-def create_properties():
-    context = bpy.context
-    #load_tile_defaults(context)
-    refresh_scene_props(context)
-    scene_props = bpy.context.scene.mt_scene_props
-
-    #scene_props['mt_materials'] = []  # list of MakeTile compatible displacement materials
-
-
 def load_tile_defaults(context):
     """Load tile defaults into memory."""
-    #scene_props = context.scene.mt_scene_props
     addon_path = get_path()
     json_path = os.path.join(
         addon_path,
@@ -120,15 +112,39 @@ def load_tile_defaults(context):
     if os.path.exists(json_path):
         with open(json_path) as json_file:
             tile_defaults = json.load(json_file)
-        #scene_props['tile_defaults'] = tile_defaults
             return tile_defaults
     return False
 
 
-def refresh_scene_props(context):
-    scene_props = context.scene.mt_scene_props
+def initialise_scene_props(context):
     prefs = get_prefs()
+    scene_props = context.scene.mt_scene_props
     scene_props.tile_type = prefs.default_tile_type
+    tile_defaults = load_tile_defaults(context)
+
+    for tile in tile_defaults:
+        if tile['type'] == scene_props.tile_type:
+            defaults = tile['defaults']
+            for key, value in defaults.items():
+                setattr(scene_props, key, value)
+
+            base_blueprint = scene_props.base_blueprint
+            base_defaults = defaults['base_defaults']
+            for key, value in base_defaults.items():
+                if key == base_blueprint:
+                    for k, v in value.items():
+                        setattr(scene_props, k, v)
+                    break
+
+            main_part_blueprint = scene_props.main_part_blueprint
+            main_part_defaults = defaults['tile_defaults']
+            for key, value in main_part_defaults.items():
+                if key == main_part_blueprint:
+                    for k, v in value.items():
+                        setattr(scene_props, k, v)
+                    break
+            break
+
 
 bpy.app.handlers.depsgraph_update_pre.append(create_properties_on_activation)
 bpy.app.handlers.load_post.append(create_properties_on_load)

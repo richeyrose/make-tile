372af02b9f0fcea6aae68701c1c91e554598c264
Richard Rose <richard.rose@modmodterrain.com>
2021-06-19 10:24:52 +0100

    create_helper_object optimised

--- operators/assign_reference_object.py
+++ operators/assign_reference_object.py
@@ -1,7 +1,6 @@
 import bpy
 from ..lib.utils.collections import add_object_to_collection, create_collection
 
-
 class MT_OT_Assign_Reference_Object(bpy.types.Operator):
     bl_idname = "material.assign_reference_object"
     bl_label = "assign_reference_object"
@@ -10,19 +9,20 @@ class MT_OT_Assign_Reference_Object(bpy.types.Operator):
         create_helper_object(context)
         return {'FINISHED'}
 
-def create_helper_object(context):
-    # Helper object collection
-    helper_collection = create_collection('MT Helpers', context.scene.collection)
 
-    # Add an empty used as a reference object for material projection
-    if 'material_helper_empty' not in bpy.data.objects:
-        material_helper = bpy.data.objects.new('material_helper_empty', None)
-    else:
+def create_helper_object(context):
+    try:
+        helper_collection = bpy.data.collections['MT_Helpers']
+    except KeyError:
+        helper_collection = create_collection('MT Helpers', context.scene.collection)
+
+    try:
         material_helper = bpy.data.objects['material_helper_empty']
+    except KeyError:
+        material_helper = bpy.data.objects.new('material_helper_empty', None)
 
     add_object_to_collection(material_helper, helper_collection.name)
     assign_obj_to_obj_texture_coords(material_helper)
-    # material_helper.hide_set(True)
     material_helper.hide_viewport = True
 
 def assign_obj_to_obj_texture_coords(obj):
